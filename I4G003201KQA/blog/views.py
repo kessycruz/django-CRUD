a5b5c50baf066d291d987e47a3604c58926bd442
kesiena <ohwotakesiena@gmail.com>
2022-06-22 03:03:27 -0700

    'django-CRUD'

---
+++ I4G003201KQA/blog/views.py
@@ -0,0 +1,27 @@
+from turtle import update
+from django.shortcuts import render
+from django.views import generic
+from .models import Post
+from django.urls import reverse_lazy
+# Create your views here.
+
+class PostListView(generic.ListView):
+    model = Post
+
+class PostCreateView(generic.CreateView):
+    model = Post
+    fields = '__all__'
+    success_url  = reverse_lazy('blog:all')
+
+class PostDetailView(generic.DetailView):
+    model = Post
+
+class PostUpdateView(generic.UpdateView):
+    model = Post
+    fields = '__all__'
+    success_url  = reverse_lazy('blog:all')
+
+class PostDeleteView(generic.UpdateView):
+    model = Post
+    fields = '__all__'
+    success_url  = reverse_lazy('blog:all')
